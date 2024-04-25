4cfa659558442f634b620fba1858088b754843a1
Pedro Andriotti <phab@cesar.school>
2024-04-24 21:18:39 -0300

    Starting Next App

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+
+export default function Home() {
+  const [data, setData] = useState<{ id: number; name: string; street: string; number: string; neighborhood: string; complement: string; }[] | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:8080/clients');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData().catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      <h1>TESTANDO A API</h1>
+      <div>
+        {data && data.map(e => (
+          <div key={e.id}>
+            <p >{e.name}</p>
+            <p >{e.street}</p>
+            <p >{e.number}</p>
+            <p >{e.neighborhood}</p>
+            <p >{e.complement}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
