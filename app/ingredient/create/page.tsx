edccd91cdb6540884cf6d9537df288c738801ef5
luismingati <luismingati@gmail.com>
2024-04-28 21:21:18 -0300

    disabled ingredient option

--- app/ingredient/create/page.tsx
+++ app/ingredient/create/page.tsx
@@ -88,7 +88,7 @@ const NewIngredient = () => {
             required
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
           >
-            <option value="">Select a unidade de medida</option>
+            <option disabled value="">Select a unidade de medida</option>
             <option value="KILOGRAM">Quilogramas</option>
             <option value="GRAM">Gramas</option>
             <option value="LITER">Litros</option>
@@ -119,7 +119,7 @@ const NewIngredient = () => {
             required
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
           > 
-            <option value="">Escolha um fornecedor</option>
+            <option disabled value="">Escolha um fornecedor</option>
             {supplier.map((supplier) => {
               return <option key={supplier.id} value={supplier.id}>{supplier.name}</option>
             })}
