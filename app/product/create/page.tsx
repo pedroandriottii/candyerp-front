116204a14e13d6dc588437a86cb737a373845fa4
luismingati <luismingati@gmail.com>
2024-05-03 11:45:39 -0300

    create product done

--- app/product/create/page.tsx
+++ app/product/create/page.tsx
@@ -16,6 +16,7 @@ const NewProduct = () => {
   const [ingredients, setIngredients] = useState<IngredientProps[]>([]);
   const [selectedIngredients, setSelectedIngredients] = useState<number[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [ingredientQuantity, setIngredientQuantity] = useState<{ [key: number]: number }>({});
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/details`)
@@ -27,11 +28,25 @@ const NewProduct = () => {
       .then(setIngredients);
   }, []);
 
+  const handleIngredientQuantityChange = (ingredientId: number, quantity: number) => {
+    setIngredientQuantity(prevQuantities => ({
+      ...prevQuantities,
+      [ingredientId]: quantity
+    }));
+  };
+
   const handleIngredientChange = (ingredientId: number) => {
     setSelectedIngredients(prev => {
       if (prev.includes(ingredientId)) {
+        const updatedQuantities = {...ingredientQuantity};
+        delete updatedQuantities[ingredientId];
+        setIngredientQuantity(updatedQuantities);
         return prev.filter(id => id !== ingredientId);
       } else {
+        setIngredientQuantity(prevQuantities => ({
+          ...prevQuantities,
+          [ingredientId]: 0
+        }));
         return [...prev, ingredientId];
       }
     });
@@ -40,7 +55,6 @@ const NewProduct = () => {
   const handleSubmit = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
     setIsLoading(true);
-
     const productResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -54,12 +68,15 @@ const NewProduct = () => {
       const { id: productId } = response;
       console.log("passei por criacao de produto id")
       console.log(productId)
+
       await Promise.all(selectedIngredients.map(ingredientId => {
+        const quantityForThisIngredient = ingredientQuantity[ingredientId];
+        console.log(JSON.stringify({ fk_Product_id: productId, fk_Ingredient_id: ingredientId, quantity: ingredientQuantity }))
         console.log(ingredientId)
         return fetch(`${process.env.NEXT_PUBLIC_API_URL}/ingredient-products`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ fk_Product_id: productId, fk_Ingredient_id: ingredientId, quantity: quantity })
+          body: JSON.stringify({ fk_Product_id: productId, fk_Ingredient_id: ingredientId, quantity: quantityForThisIngredient })
         });
       }));
 
@@ -125,16 +142,27 @@ const NewProduct = () => {
         <div>
           <label className="block text-sm font-medium text-gray-700">Ingredientes:</label>
           {ingredients.map(ingredient => (
-            <div key={ingredient.id}>
+            <div key={ingredient.id} className="flex items-center space-x-2">
               <label className="inline-flex items-center">
                 <input
                   type="checkbox"
-                  className="form-checkbox"
+                  className="h-5 w-5 text-indigo-600"
                   checked={selectedIngredients.includes(ingredient.id)}
                   onChange={() => handleIngredientChange(ingredient.id)}
                 />
                 {ingredient.name}
               </label>
+              {selectedIngredients.includes(ingredient.id) && (
+                <input
+                  type="number"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  value={ingredientQuantity[ingredient.id] || ''}
+                  onChange={e => handleIngredientQuantityChange(ingredient.id, parseFloat(e.target.value))}
+                  min="0"
+                  step="0.1"
+                />
+              )}
+              {ingredient.measurement_unit}
             </div>
           ))}
         </div>
