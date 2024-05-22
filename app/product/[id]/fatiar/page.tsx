878d82752ad6f45ef4d39835c652202606976150
luismingati <luismingati@gmail.com>
2024-05-21 22:19:29 -0300

    agora cria o produto

--- app/product/[id]/fatiar/page.tsx
+++ app/product/[id]/fatiar/page.tsx
@@ -2,8 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 import FormLabel from "@/components/form/FormLabel";
 import { ProductProps } from "@/types";
 
@@ -11,10 +9,11 @@ const FatiarProduto = ({ params }: { params: { id: string } }) => {
   const router = useRouter();
   const [product, setProduct] = useState<ProductProps>();
   const [products, setProducts] = useState<ProductProps[]>([]);
-  const [selectedSliceProduct, setSelectedSliceProduct] = useState<number>();
   const [quantity, setQuantity] = useState(0);
   const [slicesPerProduct, setSlicesPerProduct] = useState(0);
+  const [price, setPrice] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [slicesProductExists, setSlicesProductExists] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,8 +24,7 @@ const FatiarProduto = ({ params }: { params: { id: string } }) => {
 
         const allProductsResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
         const allProductsData = await allProductsResponse.json();
-        const filteredProducts = allProductsData.filter((prod: ProductProps) => prod.fk_product_id !== null);
-        setProducts(filteredProducts);
+        setProducts(allProductsData);
       } catch (error) {
         console.error("Failed to fetch data", error);
       }
@@ -35,42 +33,71 @@ const FatiarProduto = ({ params }: { params: { id: string } }) => {
     fetchData();
   }, [params.id]);
 
+  useEffect(() => {
+    if (product && quantity > 0 && slicesPerProduct > 0) {
+      const sliceProductName = `Fatia de ${product.name}`;
+      const existingProduct = products.find(prod => prod.name === sliceProductName);
+      setSlicesProductExists(!!existingProduct);
+    }
+  }, [product, quantity, slicesPerProduct, products]);
+
   const handleSlice = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
     setIsLoading(true);
 
-    if (!product || !selectedSliceProduct || quantity <= 0 || slicesPerProduct <= 0) {
-      toast('Por favor, preencha todos os campos corretamente.', { type: 'error' });
+    if (!product || quantity <= 0 || slicesPerProduct <= 0 || (!slicesProductExists && !price)) {
       setIsLoading(false);
       return;
     }
 
     const updatedProductQuantity = product.quantity - quantity;
-    const slicesProduct = products.find(prod => prod.id === selectedSliceProduct);
-    const updatedSlicesProductQuantity = (slicesProduct?.quantity ?? 0) + (quantity * slicesPerProduct);
+    const sliceProductName = `Fatia de ${product.name}`;
+    const totalSlices = quantity * slicesPerProduct;
 
     try {
+      // Atualiza a quantidade do produto original
       const productResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${product.id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ ...product, quantity: updatedProductQuantity })
       });
 
-      const slicesProductResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${selectedSliceProduct}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ ...slicesProduct, quantity: updatedSlicesProductQuantity })
-      });
-
-      if (productResponse.ok && slicesProductResponse.ok) {
-        toast('Produto fatiado com sucesso!', { type: 'success' });
-        router.push("/product");
-      } else {
-        throw new Error('Erro ao atualizar produtos');
+      if (!productResponse.ok) {
+        throw new Error('Erro ao atualizar produto original');
       }
+
+      // Verifica se o produto fatiado já existe
+      let slicesProduct = products.find(prod => prod.name === sliceProductName);
+
+      if (slicesProduct) {
+        // Produto fatiado já existe, atualiza a quantidade
+        slicesProduct.quantity += totalSlices;
+        const updateSlicesProductResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${slicesProduct.id}`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(slicesProduct)
+        });
+
+        if (!updateSlicesProductResponse.ok) {
+          throw new Error('Erro ao atualizar produto fatiado');
+        }
+      } else {
+        // Produto fatiado não existe, cria um novo
+        
+        const createSlicesProductResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name: sliceProductName, price: parseFloat(price), quantity: totalSlices, fk_product_id: product.id })
+        });
+
+        if (!createSlicesProductResponse.ok) {
+          throw new Error('Erro ao criar produto fatiado');
+        }
+      }
+
+      router.push("/product");
     } catch (error) {
-      console.error("Failed to update products", error);
-      toast('Erro ao fatiar o produto.', { type: 'error' });
+      console.error("Failed to process products", error);
     } finally {
       setIsLoading(false);
     }
@@ -83,7 +110,6 @@ const FatiarProduto = ({ params }: { params: { id: string } }) => {
   return (
     <div className="flex flex-col bg-candy-purple max-h-40 items-center p-4 w-full h-full">
       <FormLabel labelType="sliceProducts" />
-      <ToastContainer />
       <form onSubmit={handleSlice} className="space-y-4">
         <div className="flex flex-col max-w-lg gap-4 bg-white p-4 m-6 rounded-lg shadow-md">
           {product && (
@@ -121,23 +147,20 @@ const FatiarProduto = ({ params }: { params: { id: string } }) => {
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                 />
               </div>
-              <div>
-                <label htmlFor="selectedSliceProduct" className="block text-sm font-medium text-gray-700">Produto para Receber Fatias</label>
-                <select
-                  id="selectedSliceProduct"
-                  value={selectedSliceProduct || ''}
-                  onChange={(e) => setSelectedSliceProduct(parseInt(e.target.value))}
-                  required
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                >
-                  <option value="">Selecione um subproduto</option>
-                  {products.map((product) => (
-                    <option key={product.id} value={product.id}>
-                      {product.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
+              {!slicesProductExists && (
+                <div>
+                  <label htmlFor="price" className="block text-sm font-medium text-gray-700">Preço da Fatia</label>
+                  <input
+                    id="price"
+                    type="number"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    required
+                    placeholder="Preço"
+                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  />
+                </div>
+              )}
               <button
                 type="submit"
                 className="flex justify-center py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-candy-purple hover:bg-candy-purple-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
